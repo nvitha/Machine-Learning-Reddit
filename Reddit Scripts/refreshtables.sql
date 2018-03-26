257bf66bf10118bffc093ce6ab9e500f0273d8ed
Nick Vitha <nvitha@users.noreply.github.com>
2018-03-25 17:35:01 -0700

    Allow sql script running if tables not created

--- Reddit Scripts/refreshtables.sql
+++ Reddit Scripts/refreshtables.sql
@@ -1,5 +1,5 @@
-drop table posts;
-drop table comments;
+drop table if exists posts;
+drop table if exists comments;
 
 create table posts (id serial, username text, permalink text, subreddit text, title text, datetime timestamp);
 create table comments (id serial, username text, permalink text, subreddit text, body text, datetime timestamp);
